58314f84919324e3b0555f061a7f6778b6a385e8
Spieler727 <spieler0727@gmail.com>
2024-03-31 19:40:33 -0400

    fixed printing errors

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from PIL import Image
 
 def callback(win_handle, titles_list):
     if win32gui.IsWindowVisible(win_handle):
-        title = win32gui.GetWindowText(win_handle)
-        titles_list.append(title)
+        try:
+            title = win32gui.GetWindowText(win_handle)
+            if all(ord(character) < 128 for character in title):
+                titles_list.append(title)
+        
+        except:
+            return None
         
 def get_window_titles():
     window_titles = []
 
     # Passes the handle of every top-level window and the window_titles list to callback function
     win32gui.EnumWindows(callback, window_titles)
-
+    
     return window_titles
 
 list = get_window_titles()
@@ -24,11 +29,22 @@ def check_for_ldplayer(windows_list):
     if "LDPlayer" in windows_list:
         print("LDPlayer found")
         ldplayer_window_handle = win32gui.FindWindow(None, "LDPlayer")
-        print(ldplayer_window_handle)
+        return ldplayer_window_handle
         
     else:
         print("LDPlayer not found")
-        return
+        return None
     
         
-check_for_ldplayer(list)
+ld_handle = check_for_ldplayer(list)
+
+def capture_ldplayer_screenshot(ldplayer_handle):
+    if ldplayer_handle:
+        rect = win32gui.GetWindowRect(ldplayer_handle)
+        x, y, width, height = rect
+        ld_screenshot = pyautogui.screenshot(region=(x, y, width, height))
+        return np.array(ld_screenshot)
+    
+    else:
+        print("LDPlayer not found")
+        return None
