94dabc4ce4839617585cd4f3647cb8b2a6670b84
Spieler727 <spieler0727@gmail.com>
2024-04-10 02:35:59 -0400

    returning to template matching

--- main.py
+++ main.py
@@ -9,28 +9,20 @@ def main():
     ld = capture_ldplayer_screenshot(ld_handle)
 
     template_folder_path = 'templates'
-    template_image_path = os.path.join(template_folder_path, 'castoria_skill1.png')
-    template = cv2.imread(template_image_path, cv2.IMREAD_COLOR)
+    template_image_path = os.path.join(template_folder_path, 's1.png')
+    template = cv2.imread(template_image_path, cv2.IMREAD_GRAYSCALE)
     screen_path = os.path.join(template_folder_path, 'screen1.png')
-    screen = cv2.imread(screen_path, cv2.IMREAD_COLOR)
+    screen = cv2.imread(screen_path, cv2.IMREAD_GRAYSCALE)
 
-    # Test Execution
-    template_corners = detect_corners(template)
-    screenshot_corners = detect_corners(screen)
-    #cv2.imshow('Template Corners', template_corners)
-    cv2.imshow('Screenshot Corners', screenshot_corners)
+   
+    matches = find_template_match(screen, template)
+    print("The matches are:", matches)
+    
 
-    template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    screen_gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-
-    # Initialize ORB detector
-    matched_image = detect_and_match_sift(template, screen)
-
-    # Draw the top matches
     #matched_image = cv2.drawMatches(template, kp1, screen, kp2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     
     # Display the matched image
-    cv2.imshow('Feature Matching Result', matched_image)
+    
 
     # Cleanup
     print("This is my cursor's position:", pyautogui.position())
